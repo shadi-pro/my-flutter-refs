448fb58ca2a7ed4e58b6c8478c98018fe6c31748
shadi-pro <shadisayed.6890@gmail.com>
2025-11-29 00:36:52 +0200

    task manager app backend 3

---
+++ demos/backup verions/task manager app/backend/v3 addtional features/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'pages/home_page.dart';
+import 'pages/add_task_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const TaskManagerApp());
+}
+
+class TaskManagerApp extends StatelessWidget {
+  const TaskManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/add': (context) => const AddTaskPage(),
+      },
+    );
+  }
+}
